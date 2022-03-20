4e21931bb09d269054cd1b1ba82dd6381c1ffff0
Utkarsh Upendra <71965330+Utkarsh3012@users.noreply.github.com>
2022-03-20 22:48:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+import pytesseract
+import os
+import cv2
+
+# Enter the location of tesseract.exe in the "_"
+pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR/tesseract.exe"
+
+# enter the location/path of image below
+imagepath = r"sample (1).jpg"
+img = cv2.imread(
+    imagepath)
+
+# configuring tesseract for language detection
+configuration = ("-l eng --oem 1 --psm 8")
+
+# extracting text from image
+text = pytesseract.image_to_string(
+    img, lang="eng", config=configuration)
+
+# saving text in a file output.txt(has to be premade in the folder)
+file = open("OP.txt", "a")
+file.write(text)
+file.close()
+
+print("Text extracted has been saved in O/P.txt!")
